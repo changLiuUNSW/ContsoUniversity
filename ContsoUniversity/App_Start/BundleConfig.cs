c9ec38be6b4c52e6bac9fef07a60205375908e78
Chang Liu <3dslayer@gmail.com>
2014-05-05 21:00:14 +1000

    Update project

---
+++ ContsoUniversity/App_Start/BundleConfig.cs
@@ -0,0 +1,23 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace ContsoUniversity
+{
+    public class BundleConfig
+    {
+        // For more information on Bundling, visit http://go.microsoft.com/fwlink/?LinkId=254725
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.IgnoreList.Clear();
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/Scripts/jquery-{version}.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                        "~/Scripts/bootstrap.min.js"));
+
+            bundles.Add(new StyleBundle("~/Content/bootstrapcss").Include(
+                        "~/Content/bootstrap.min.css",
+                        "~/Content/bootstrap-responsive.min.css"));
+        }
+    }
+}
---
+++ ContsoUniversity/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ContsoUniversity.MvcApplication" Language="C#" %>
